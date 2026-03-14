51681baf894542c2ad2a4474e108466d7ab0d7e4
Shourya Eaga <shourya.eaga.09@gmail.com>
2026-03-14 18:57:26 +0000

    Updating folder structure

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ import matplotlib.pyplot as plt
 mlp= NeuralNetwork(input_size=784, output_size=10, hidden_layer_size=16, num_hidden_layers=2, softmax=True)
 mlp.create_architecture()
 
-weight_layer_1 = np.loadtxt("net_1_weight.csv", delimiter=",")
-bias_layer_1 = np.loadtxt("net_1_bias.csv", delimiter=",")
-weight_layer_3 = np.loadtxt("net_3_weight.csv", delimiter=",")
-bias_layer_3 = np.loadtxt("net_3_bias.csv", delimiter=",")
-weight_layer_5 = np.loadtxt("net_5_weight.csv", delimiter=",")
-bias_layer_5 = np.loadtxt("net_5_bias.csv", delimiter=",")
+weight_layer_1 = np.loadtxt("pretrained_pt_weights/net_1_weight.csv", delimiter=",")
+bias_layer_1 = np.loadtxt("pretrained_pt_weights/net_1_bias.csv", delimiter=",")
+weight_layer_3 = np.loadtxt("pretrained_pt_weights/net_3_weight.csv", delimiter=",")
+bias_layer_3 = np.loadtxt("pretrained_pt_weights/net_3_bias.csv", delimiter=",")
+weight_layer_5 = np.loadtxt("pretrained_pt_weights/net_5_weight.csv", delimiter=",")
+bias_layer_5 = np.loadtxt("pretrained_pt_weights/net_5_bias.csv", delimiter=",")
 mlp.set_layer_weights(weight_layer_1, 0)
 mlp.set_layer_weights(weight_layer_3, 2)
 mlp.set_layer_weights(weight_layer_5, 4)
@@ -19,7 +19,8 @@ mlp.set_layer_bias(bias_layer_1, 0)
 mlp.set_layer_bias(bias_layer_3, 2)
 mlp.set_layer_bias(bias_layer_5, 4)
 
-mnist = MnistDataLoader("train-images.idx3-ubyte", "train-labels.idx1-ubyte", "t10k-images.idx3-ubyte", "t10k-labels.idx1-ubyte")
+mnist = MnistDataLoader("MNIST/train-images.idx3-ubyte", "MNIST/train-labels.idx1-ubyte",
+                        "MNIST/t10k-images.idx3-ubyte", "MNIST/t10k-labels.idx1-ubyte")
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
 image = X_train[1]
