bd477f93146842d1f6555a9ecc2a5c2aa5889ca9
Shourya Eaga <shourya.eaga.09@gmail.com>
2026-03-14 13:39:06 +0000

    Cleaning up class structure, creating a base layer class, and ReLU. Linear and ReLU both are children of Layer

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,5 +1,13 @@
 import numpy as np
-class Linear:
+
+class Layer:
+    def forward(self, X):
+        raise NotImplementedError
+
+    def backprop(self, grad):
+        raise NotImplementedError
+
+class Linear(Layer):
     def __init__(self, input_size, output_size: int):
         self.activations: np.ndarray = np.random.rand(input_size)
         self.W: np.ndarray = np.random.rand(output_size, input_size)
@@ -8,19 +16,18 @@ class Linear:
 
     def forward(self, activation_previous: np.ndarray) -> np.ndarray:
         self.activation_previous = activation_previous
-        self.activations = np.dot(self.W, activation_previous) + self.b
+        print(np.transpose(self.activation_previous).shape)
+        self.activations = np.matmul(self.W, np.transpose(activation_previous)) + np.transpose(self.b)
         return self.activations
 
-# class Weights:
-#     def __init__(self, layer_left_size: int, layer_right_size: int):
-#         self.layer_left_size = layer_left_size
-#         self.layer_right_size = layer_right_size
-#         # first row is for the first node on the right, and its weights for all the neurones on the left
-#         self.weights: np.ndarray = np.random.rand(layer_right_size, layer_left_size)
+class ReLU(Layer):
+    def forward(self, X):
+        self.mask = X > 0
+        return X * self.mask
 
 class NeuralNetwork:
     def __init__(self, hidden_layer_size: int, output_size: int, input_size: int, num_hidden_layers: int):
-        self.layers: list[Linear] = []
+        self.layers: list[Layer] = []
         if num_hidden_layers < 1:
             raise ValueError("num_hidden_layers must be greater than or equal to 1")
         self.hidden_layer_size: int = hidden_layer_size
@@ -39,5 +46,6 @@ class NeuralNetwork:
             print(layer.activations)
 
     def forward(self, X: np.ndarray) -> np.ndarray:
-        for layer, weight in zip(self.layers, self.weights):
-            pass
+        for layer in self.layers:
+            X = layer.forward(X)
+        return X
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from NeuralNetwork import NeuralNetwork
+import numpy as np
 
 mlp= NeuralNetwork(input_size=28, output_size=10, hidden_layer_size=16, num_hidden_layers=2)
 mlp.create_architecture()
-mlp.print_activations()
+# mlp.print_activations()
+image = np.random.random(28)
+print(mlp.forward(image))
